8774ff5edd17af6b18ab51f022f74b3400c3db15
naomicrisp <naomicrisp2@gmail.com>
2023-04-25 14:21:02 +0100

    drop down added

--- study-app/src/App/App.js
+++ study-app/src/App/App.js
@@ -2,6 +2,16 @@ import React from 'react';
 import './App.css';
 import List from '../List/List';
 import ListItem from '../ListItem/ListItem';
+import DropDown from '../DropDown/DropDown';
+
+//PLAN
+
+//DONE // create working dropdown menu component // test click-off functionality 
+// write out options of dropdown menu and make sure they display/ are selectable 
+// write dummy data to display to list
+// make function to attach to dropdown so that when an option is selected it displays to list
+// check that dropdown menu displays data to the list 
+// chanage the type of data being displayed so it is similar to our actual data
 
 
 function App() {
@@ -9,11 +19,10 @@ function App() {
   
   
   
-  
-  
   return (
     <div>
       <h1>Test</h1>
+      <DropDown />
       <List />
       <ListItem />
     </div>
