ce57d62365e6902f39ef050484fdf052cf49e8f7
James Attrill <attrill20@gmail.com>
2023-04-27 18:59:04 +0100

    two sections done

--- study-app/src/App/App.js
+++ study-app/src/App/App.js
@@ -17,7 +17,7 @@ const dummyData = {
     { 
       title: "React Component Guide", 
       youtubeUrl: "9vS86xgG3ak", 
-      docUrl:`<p>One of the best ways to fully understand the specifics of components is to fully read the official docs from React.</p>
+      docUrl:`<p>One of the best ways to fully understand the specifics of components is to fully read the official Documentation from React.</p>
       <p>Click <a href="https://react.dev/learn/your-first-component" target="_blank">here</a> for the Docs!</p>
       <p>Make sure to try out the Challenges at the bottom of the page too and put your newly found skills into practice.</p>`,
       quizUrl:`<p>Once you feel like you have leared the basics of this section, testing your knowledge with a quiz is a great way of checking your progress.</p>
@@ -37,16 +37,24 @@ const dummyData = {
     { 
       title: "Props", 
       youtubeUrl: "PHaECbrKgs0", 
-      docUrl: `click <a href="https://www.freecodecamp.org/news/how-to-use-props-in-react/">here</a> for the docs`, 
-      quizUrl: `click <a href="https://quizgecko.com/create?quiz_id=40441">here</a> for the quiz` ,
-      textContent: `<p>React Components Components are independent and reusable bits of code. They serve the same purpose as JavaScript functions, but work in isolation and return HTML.</p>
-      <p>Introduction</p>
-      <p>A component is the probably most important concept to understand in React. It is one of the core building blocks of React that allows us to split a UI into independent, reusable pieces, making the task of building UIs much easier. All of these independent components are then combined into a parent component, which will serve as our final user interface.</p>
-      <p>In this guide, we will take a look at components and how they work in React, we will also see how to pass data from one component to another using props.</p> 
-      <p>What Is a Component?</p>
-      <p>A component is a self-contained, reusable code block that divides the user interface into smaller pieces rather than building the entire UI in a single file. Buttons, forms, dialog, and so on can all be expressed as components. React components accept arbitrary inputs ("props") and return React elements or JSX code that tells what should be rendered on the screen.</p>
-      <p>To demonstrate how components work, we have broken down the UI of an example Todo Application into individual components:
+      docUrl: `<p>One of the best ways to fully understand the specifics of components is to fully read the official Documentation from React.</p>
+      <p>Click <a href="https://react.dev/learn/passing-props-to-a-component" target="_blank">here</a> for the Docs!</p>
+      <p>Make sure to try out the Challenges at the bottom of the page too and put your newly found skills into practice.</p>`, 
+      quizUrl: `<p>Once you feel like you have leared the basics of this section, testing your knowledge with a quiz is a great way of checking your progress.</p>
+      <p>Click <a href="https://quizgecko.com/create?quiz_id=40441" target="_blank">here</a> for the quiz!</p>
+      <p>These results will be tracked on your My Progress page - best of luck!</p>`,
+      textContent: `<p>Props are used to store data that can be accessed by the children of a React component. They are part of the concept of reusability. Props take the place of class attributes and allow you to create consistent interfaces across the component hierarchy.</p>
+      <p><strong>What Are Props in React?</strong></p>
+      <p>Props simply stands for properties. They are what make components reusable. Because they perform an essential function - they pass data from one component to another.</p> 
+      <p>Props act as a channel for component communication. Props are passed from parent to child and help your child access properties that made it into the parent's tree.</p>
+      <p><strong>Things worth noting:</strong></p>
+      <ul>We use props in both functional and class-based components.</ul>
+      <ul>We pass props from the top to bottom. We can also refer to it as ancestor to descendant, or parent to child.</ul>
+      <ul>Props are read-only. This means that once a component receives a bunch of props, we cannot change it, but we can only use and consume it and cannot modify the properties passed down to the component. If we want to modify that, we'll have to introduce what we call state in React.</ul>
+      <p>To learn more about components, click <a href="https://www.w3schools.com/react/react_props.asp" target="_blank">here</a>.
       </p>`
+
+
     }
   ],
   Hooks: [
