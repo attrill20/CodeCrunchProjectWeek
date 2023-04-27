1411e4e1410146a49741d4df7e4cbefae0e85238
safiyahl <safiyah.seedat@gmail.com>
2023-04-27 14:59:27 +0100

    progress page updated

--- study-app/src/Footer/Footer.js
+++ study-app/src/Footer/Footer.js
@@ -1,75 +1,78 @@
-import React from 'react';
-
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebookF,
+  faTwitter,
+  faGoogle,
+  faInstagram,
+  faLinkedinIn,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
 const Footer = () => {
-    return (
-<footer className="bg-light text-center text-white" style={{"position":"relative", "bottom": "0"}}>
-
-  <div className="container p-4 pb-0">
-    
-    {/* <section className="mb-4">
-      
-      <a
-        className="btn text-white btn-floating m-1"
-        // style="background-color: #3b5998;"
-        href="#!"
-        role="button"
-        ><i className="fab fa-facebook-f"></i
-      ></a>
-
-      
-      <a
-        className="btn text-white btn-floating m-1"
-        // style="background-color: #55acee;"
-        href="#!"
-        role="button"
-        ><i className="fab fa-twitter"></i
-      ></a>
-
-     
-      <a
-        className="btn text-white btn-floating m-1"
-        // style="background-color: #dd4b39;"
-        href="#!"
-        role="button"
-        ><i className="fab fa-google"></i
-      ></a>
-
-      <a
-        className="btn text-white btn-floating m-1"
-        // style="background-color: #ac2bac;"
-        href="#!"
-        role="button"
-        ><i className="fab fa-instagram"></i
-      ></a>
-
-     
-      <a
-        className="btn text-white btn-floating m-1"
-        // style="background-color: #0082ca;"
-        href="#!"
-        role="button"
-        ><i className="fab fa-linkedin-in"></i
-      ></a>
-      
-      <a
-        className="btn text-white btn-floating m-1"
-        style={{"background-color": "#333333"}}
-        href="#!"
-        role="button"
-        ><i className="fab fa-github"></i
-      ></a>
-    </section> */}
-   
-  </div>
-
-
- 
-  <div className="text-center p-3" style={{"background-color": "#9686AC"}}>
-    Crunch your code, not your time!    © 2023
-  </div>
-  
-</footer>
-);
+  return (
+    <footer
+      className="bg-light text-center text-white"
+      style={{ position: "relative", bottom: "0" }}
+    >
+      <div className="container p-4 pb-0">
+        <section
+          className="mb-4 d-flex justify-content-center align-items-center"
+          style={{ marginTop: "15px" }}
+        >
+          <a
+            className="btn text-white btn-floating m-1"
+            style={{ backgroundColor: "#3B5998", padding: "5px" }}
+            href="#!"
+            role="button"
+          >
+            <FontAwesomeIcon icon={faFacebookF} size="2x" />
+          </a>
+          <a
+            className="btn text-white btn-floating m-1"
+            style={{ backgroundColor: "#55ACEE", padding: "5px" }}
+            href="#!"
+            role="button"
+          >
+            <FontAwesomeIcon icon={faTwitter} size="2x" />
+          </a>
+          <a
+            className="btn text-white btn-floating m-1"
+            style={{ backgroundColor: "#DD4B39", padding: "5px" }}
+            href="#!"
+            role="button"
+          >
+            <FontAwesomeIcon icon={faGoogle} size="2x" />
+          </a>
+          <a
+            className="btn text-white btn-floating m-1"
+            style={{ backgroundColor: "#AC2BAC", padding: "5px" }}
+            href="#!"
+            role="button"
+          >
+            <FontAwesomeIcon icon={faInstagram} size="2x" />
+          </a>
+          <a
+            className="btn text-white btn-floating m-1"
+            style={{ backgroundColor: "#0082CA", padding: "5px" }}
+            href="#!"
+            role="button"
+          >
+            <FontAwesomeIcon icon={faLinkedinIn} size="2x" />
+          </a>
+          <a
+            className="btn text-white btn-floating m-1"
+            style={{ backgroundColor: "#333333", padding: "5px" }}
+            href="#!"
+            role="button"
+          >
+            <FontAwesomeIcon icon={faGithub} size="2x" />
+          </a>
+        </section>
+      </div>
+      <div className="text-center p-3" style={{ backgroundColor: "#9686AC" }}>
+        Crunch your code, not your time! &copy; 2023
+      </div>
+    </footer>
+  );
 };
-
 export default Footer;
--- study-app/src/ProgressPage/ProgressPage.js
+++ study-app/src/ProgressPage/ProgressPage.js
@@ -1,13 +1,39 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import ProgressBar from "react-bootstrap/ProgressBar";
+import "../App/App.css";
+const mockUserProgress = {
+  quizzes: [
+    { id: 1, name: "Quiz 1", score: 80, topic: "React" },
+    { id: 2, name: "Quiz 2", score: 90, topic: "Components" },
+    { id: 3, name: "Quiz 3", score: 75, topic: "Props" },
+  ],
+  completedTopics: ["React", "Components", "Props", "Hooks"],
+};
 function ProgressPage() {
+  const progress = 75;
   return (
     <div className="ProgressPage">
       <h1>My Progress</h1>
+      <ProgressBar now={progress} label={`${progress}%`} />
+      <h4>Quiz Scores:</h4>
+      <ul>
+        {mockUserProgress.quizzes.map((quiz) => (
+          <li key={quiz.id}>
+            {`Quiz ${quiz.id} - ${quiz.score}% - Topic: ${quiz.topic}`}
+            <br />
+            Topic: {quiz.topic}
+            <br />
+            {mockUserProgress.completedTopics.includes(quiz.topic) ? (
+              <span>Completed</span>
+            ) : (
+              <span>Incomplete</span>
+            )}
+          </li>
+        ))}
+      </ul>
       <Link to="/">Go Back</Link>
     </div>
   );
 }
-
 export default ProgressPage;
